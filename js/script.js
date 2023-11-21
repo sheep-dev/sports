caf80fac66cdeb1630cecc25638806e32178f516
sheep-dev <im.sheep.dev@gmail.com>
2023-11-21 18:07:36 +0800

    done fiesta conference tabs

--- js/script.js
+++ js/script.js
@@ -5,16 +5,17 @@ dropdowns.forEach(dropdown => {
   const btn = dropdown.querySelector('.standing-btn');
   const content = dropdown.querySelector('.dropdown-standing-content');
 
-  // Set initial display style to 'block'
+
   content.style.display = 'block';
 
-  btn.addEventListener('click', function() {
+  btn.addEventListener('click', ()=> {
     if (content.style.display === 'block') {
       content.style.display = 'none';
     } else {
       content.style.display = 'block';
     }
   });
+
 });
 
 
@@ -52,7 +53,7 @@ tabButtons.forEach(button => {
     tabContents.forEach(content => {
       content.style.display = 'none';
     });
-    document.getElementById(tabId).style.display = 'block';
+    document.getElementById(tabId).style.display = 'flex';
   });
 });
 
@@ -106,26 +107,6 @@ function nextDate() {
 
 // Initial display
 displayDate();
-
-
-$(document).ready(function() {
-  // Show notification on desktop refresh
-  if ($(window).width() > 600) {
-      $('#notification').show();
-  }
-
-  // Hide notification on close button click
-  $('#closeBtn').click(function() {
-      $('#notification').hide();
-  });
-
-  // Show notification on scroll for mobile
-  if ($(window).width() <= 600) {
-      $(window).scroll(function() {
-          $('#notification').show();
-      });
-  }
-});
 
 
 // NOTIFICATION
