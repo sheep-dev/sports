d8b3c086b2c0b081f1ace48f266430cae04b3db0
sheep-dev <im.sheep.dev@gmail.com>
2023-11-29 14:47:50 +0800

    tried apis

--- js/api.js
+++ js/api.js
@@ -1,10 +1,32 @@
-fetch('https://reqres.in/api/users', {
-    method: 'POST',
-    body: {
-        name: 'User 1'
-    }
-}).then(res => {
-    return res.json()
-})
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
+// fetch('https://reqres.in/api/users', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify ({
+//         name: 'User 1'
+//     })
+// }).then(res => {
+//     return res.json()
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log('ERROR'))
+
+
+// const axios = require('axios');
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://api-basketball.p.rapidapi.com/timezone',
+//   headers: {
+//     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+//     'X-RapidAPI-Host': 'api-basketball.p.rapidapi.com'
+//   }
+// };
+
+// try {
+// 	const response = await axios.request(options);
+// 	console.log(response.data);
+// } catch (error) {
+// 	console.error(error);
+// }
